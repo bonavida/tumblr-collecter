fcd57e5d595a32701592da65788e741a6b6228da
Diego <al095660@uji.es>
2016-10-28 20:26:38 +0200

    Created basic class to handle HTTP GET requests

---
+++ src/main/java/utils/Request.java
@@ -0,0 +1,46 @@
+package utils;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * A class that represents an HTTP Request
+ */
+public class Request {
+
+    public Request() {}
+
+    /**
+     * HTTP GET Request
+     * @param myUrl - URL to make the HTTP request
+     * @return the HTTP response in JSON format
+     * @throws Exception
+     */
+    public JsonElement sendGet(String myUrl) throws Exception {
+
+        try {
+            URL urlObj = new URL(myUrl);
+            HttpURLConnection request = (HttpURLConnection) urlObj.openConnection();
+
+            // Optional - default method is already GET
+            request.setRequestMethod("GET");
+
+            request.connect();
+
+            InputStream is = request.getInputStream();
+            JsonElement jsonResponse = new JsonParser().parse(new InputStreamReader(is));
+
+            request.disconnect();
+
+            return jsonResponse;
+
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+}
